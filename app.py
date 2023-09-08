f0adc32e859a74ac22d48a31bcc2bafe4e6851bd
BharatKumar2204 <86950293+BharatKumar2204@users.noreply.github.com>
2023-09-08 16:24:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 import string
 from flask_restful import Api
 from flask_cors import CORS
-app = Flask(__name__)
+app = Flask(__name__,static_url_path='', static_folder='frontend/build')
 CORS(app) #comment this on deployment
 api = Api(app)
 # ...
